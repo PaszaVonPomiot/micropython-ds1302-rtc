7601c6ab0ed8ca8a0d986cfd3eb4763c96d32164
PaszaVonPomiot <schedar@op.pl>
2025-04-26 01:52:45 +0200

    Refactor driver code and add type annotations

--- lib/ds1302/ds1302.py
+++ lib/ds1302/ds1302.py
@@ -1,117 +1,117 @@
-from machine import Pin
+from machine import Pin  # type: ignore[import]
 
-DS1302_REG_SECOND = 0x80
-DS1302_REG_MINUTE = 0x82
-DS1302_REG_HOUR = 0x84
-DS1302_REG_DAY = 0x86
-DS1302_REG_MONTH = 0x88
-DS1302_REG_WEEKDAY = 0x8A
-DS1302_REG_YEAR = 0x8C
-DS1302_REG_WP = 0x8E
-DS1302_REG_CTRL = 0x90
-DS1302_REG_RAM = 0xC0
-
+class _Reg:  # 8-bit register addresses
+    SECOND = 0x80
+    MINUTE = 0x82
+    HOUR = 0x84
+    DAY = 0x86
+    MONTH = 0x88
+    WEEKDAY = 0x8A
+    YEAR = 0x8C
+    WP = 0x8E  # Write protect register
+    CTRL = 0x90  # Control register
+    RAM = 0xC0  # RAM register (0xC0-0xFF)
 
 class DS1302:
-    def __init__(self, clk, dio, cs):
-        self.clk = clk
-        self.dio = dio
-        self.cs = cs
-        self.clk.init(Pin.OUT)
-        self.cs.init(Pin.OUT)
+    def __init__(self, *, clk: Pin, dat: Pin, rst: Pin) -> None:
+        self.pin_clk = clk
+        self.pin_dat = dat
+        self.pin_rst = rst
+        self.pin_clk.init(Pin.OUT)
+        self.pin_rst.init(Pin.OUT)
 
-    def _dec2hex(self, dat):
-        return (dat // 10) * 16 + (dat % 10)
+    def _dec2bcd(self, value: int) -> int:
+        return (value // 10) * 16 + (value % 10)
 
-    def _hex2dec(self, dat):
-        return (dat // 16) * 10 + (dat % 16)
+    def _bcd2dec(self, value: int) -> int:
+        return (value // 16) * 10 + (value % 16)
 
-    def _write_byte(self, dat):
-        self.dio.init(Pin.OUT)
+    def _write_byte(self, data_byte: int) -> None:
+        self.pin_dat.init(Pin.OUT)
         for i in range(8):
-            self.dio.value((dat >> i) & 1)
-            self.clk.value(1)
-            self.clk.value(0)
+            self.pin_dat.value((data_byte >> i) & 1)
+            self.pin_clk.value(1)
+            self.pin_clk.value(0)
 
-    def _read_byte(self):
-        d = 0
-        self.dio.init(Pin.IN)
+    def _read_byte(self) -> int:
+        data_byte = 0
+        self.pin_dat.init(Pin.IN)
         for i in range(8):
-            d = d | (self.dio.value() << i)
-            self.clk.value(1)
-            self.clk.value(0)
-        return d
+            data_byte |= self.pin_dat.value() << i
+            self.pin_clk.value(1)
+            self.pin_clk.value(0)
+        return data_byte
 
-    def _get_reg(self, reg):
-        self.cs.value(1)
-        self._write_byte(reg)
-        t = self._read_byte()
-        self.cs.value(0)
-        return t
+    def _get_register(self, register: int) -> int:
+        self.pin_rst.value(1)
+        self._write_byte(register)
+        data_byte = self._read_byte()
+        self.pin_rst.value(0)
+        return data_byte
 
-    def _set_reg(self, reg, dat):
-        self.cs.value(1)
-        self._write_byte(reg)
-        self._write_byte(dat)
-        self.cs.value(0)
+    def _set_register(self, register: int, value: int) -> None:
+        self.pin_rst.value(1)
+        self._write_byte(register)
+        self._write_byte(value)
+        self.pin_rst.value(0)
 
-    def _wr(self, reg, dat):
-        self._set_reg(DS1302_REG_WP, 0)
-        self._set_reg(reg, dat)
-        self._set_reg(DS1302_REG_WP, 0x80)
+    def _wr(self, register: int, value: int) -> None:
+        self._set_register(_Reg.WP, 0)
+        self._set_register(register, value)
+        self._set_register(_Reg.WP, 0x80)
 
-    def start(self):
-        t = self._get_reg(DS1302_REG_SECOND + 1)
-        self._wr(DS1302_REG_SECOND, t & 0x7F)
+    def start(self) -> None:
+        sec = self._get_register(_Reg.SECOND + 1)
+        self._wr(_Reg.SECOND, sec & 0x7F)
 
-    def stop(self):
-        t = self._get_reg(DS1302_REG_SECOND + 1)
-        self._wr(DS1302_REG_SECOND, t | 0x80)
+    def stop(self) -> None:
+        sec = self._get_register(_Reg.SECOND + 1)
+        self._wr(_Reg.SECOND, sec | 0x80)
 
-    def second(self, second=None):
-        if second == None:
-            return self._hex2dec(self._get_reg(DS1302_REG_SECOND + 1)) % 60
-        else:
-            self._wr(DS1302_REG_SECOND, self._dec2hex(second % 60))
+    def second(self, value: int | None = None) -> int | None:
+        if value is None:
+            return self._bcd2dec(self._get_register(_Reg.SECOND + 1)) % 60
+        self._wr(_Reg.SECOND, self._dec2bcd(value % 60))
+        return None
 
-    def minute(self, minute=None):
-        if minute == None:
-            return self._hex2dec(self._get_reg(DS1302_REG_MINUTE + 1))
-        else:
-            self._wr(DS1302_REG_MINUTE, self._dec2hex(minute % 60))
+    def minute(self, value: int | None = None) -> int | None:
+        if value is None:
+            return self._bcd2dec(self._get_register(_Reg.MINUTE + 1))
+        self._wr(_Reg.MINUTE, self._dec2bcd(value % 60))
+        return None
 
-    def hour(self, hour=None):
-        if hour == None:
-            return self._hex2dec(self._get_reg(DS1302_REG_HOUR + 1))
-        else:
-            self._wr(DS1302_REG_HOUR, self._dec2hex(hour % 24))
+    def hour(self, value: int | None = None) -> int | None:
+        if value is None:
+            return self._bcd2dec(self._get_register(_Reg.HOUR + 1))
+        self._wr(_Reg.HOUR, self._dec2bcd(value % 24))
+        return None
 
-    def weekday(self, weekday=None):
-        if weekday == None:
-            return self._hex2dec(self._get_reg(DS1302_REG_WEEKDAY + 1))
-        else:
-            self._wr(DS1302_REG_WEEKDAY, self._dec2hex(weekday % 8))
+    def weekday(self, value: int | None = None) -> int | None:
+        if value is None:
+            return self._bcd2dec(self._get_register(_Reg.WEEKDAY + 1))
+        self._wr(_Reg.WEEKDAY, self._dec2bcd(value % 8))
+        return None
 
-    def day(self, day=None):
-        if day == None:
-            return self._hex2dec(self._get_reg(DS1302_REG_DAY + 1))
-        else:
-            self._wr(DS1302_REG_DAY, self._dec2hex(day % 32))
+    def day(self, value: int | None = None) -> int | None:
+        if value is None:
+            return self._bcd2dec(self._get_register(_Reg.DAY + 1))
+        self._wr(_Reg.DAY, self._dec2bcd(value % 32))
+        return None
 
-    def month(self, month=None):
-        if month == None:
-            return self._hex2dec(self._get_reg(DS1302_REG_MONTH + 1))
-        else:
-            self._wr(DS1302_REG_MONTH, self._dec2hex(month % 13))
+    def month(self, value: int | None = None) -> int | None:
+        if value is None:
+            return self._bcd2dec(self._get_register(_Reg.MONTH + 1))
+        self._wr(_Reg.MONTH, self._dec2bcd(value % 13))
+        return None
 
-    def year(self, year=None):
-        if year == None:
-            return self._hex2dec(self._get_reg(DS1302_REG_YEAR + 1)) + 2000
-        else:
-            self._wr(DS1302_REG_YEAR, self._dec2hex(year % 100))
+    def year(self, value: int | None = None) -> int | None:
+        if value is None:
+            return self._bcd2dec(self._get_register(_Reg.YEAR + 1)) + 2000
+        self._wr(_Reg.YEAR, self._dec2bcd(value % 100))
+        return None
 
-    def date_time(self, dat=None):
-        if dat == None:
+    def date_time(self, datetime: list[int|None] | None = None) -> list[int|None] | None:
+        if datetime is None:
             return [
                 self.year(),
                 self.month(),
@@ -121,17 +121,18 @@ class DS1302:
                 self.minute(),
                 self.second(),
             ]
-        else:
-            self.year(dat[0])
-            self.month(dat[1])
-            self.day(dat[2])
-            self.weekday(dat[3])
-            self.hour(dat[4])
-            self.minute(dat[5])
-            self.second(dat[6])
+        self.year(datetime[0])
+        self.month(datetime[1])
+        self.day(datetime[2])
+        self.weekday(datetime[3])
+        self.hour(datetime[4])
+        self.minute(datetime[5])
+        self.second(datetime[6])
+        return None
 
-    def ram(self, reg, dat=None):
-        if dat == None:
-            return self._get_reg(DS1302_REG_RAM + 1 + (reg % 31) * 2)
-        else:
-            self._wr(DS1302_REG_RAM + (reg % 31) * 2, dat)
+    def ram(self, register: int, value: int | None = None) -> int | None:
+        addr = _Reg.RAM + (register % 31) * 2
+        if value is None:
+            return self._get_register(addr + 1)
+        self._wr(addr, value)
+        return None
